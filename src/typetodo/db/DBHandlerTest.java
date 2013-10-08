b0db82c4fb693e66b22269477e64dfcd696b9fee
Dennis Ng <Dennis Ng@localhost>
2013-10-08 18:03:28 +0800

    Added support for adding a task that already has a taskId

--- src/typetodo/db/DBHandlerTest.java
+++ src/typetodo/db/DBHandlerTest.java
@@ -22,18 +22,21 @@ public class DBHandlerTest {
 	public void test() {
 		DBHandler db;
 		try {
-			FloatingTask floatingTask = new FloatingTask("Name", "Desc");
-			FloatingTask floatingTask2 = new FloatingTask("Name", "Desc");
+			FloatingTask floatingTask = new FloatingTask("floatingTask1", "Desc");
+			FloatingTask floatingTask2 = new FloatingTask("floatingTask2", "Desc");
 			Date date = new Date();
-			DeadlineTask deadlineTask = new DeadlineTask(1, "Name", "Desc", date);
+			DeadlineTask deadlineTask = new DeadlineTask(1, "deadlineTask", "Desc",
+					date);
 			db = new DBHandler();
 			assertEquals("Add task", 1, db.addTask(floatingTask));
-			assertEquals("Add task", 2, db.addTask(floatingTask2));
+			assertEquals("Add task twice", 2, db.addTask(floatingTask2));
 			assertEquals("Update task", true, db.updateTask(deadlineTask));
 			assertEquals("Retreive", date,
 					((DeadlineTask) db.retrieveList(date).get(0)).getDeadline());
 			assertEquals("Delete", true, (db.deleteTask(1)));
 			assertEquals("Delete", true, (db.deleteTask(2)));
+			assertEquals("Undo deleted task", 1, (db.addTask(deadlineTask)));
+			assertEquals("Delete", true, (db.deleteTask(1)));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
