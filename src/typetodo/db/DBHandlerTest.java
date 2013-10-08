c613a7ed99f8d197b1ca60d7f774ff8dc97025d3
Dennis Ng <Dennis Ng@localhost>
2013-10-08 17:10:17 +0800

    Fixed addTask() wrong indexing and amend junit test to work without testing the reading of file (push again)

--- src/typetodo/db/DBHandlerTest.java
+++ src/typetodo/db/DBHandlerTest.java
@@ -23,14 +23,17 @@ public class DBHandlerTest {
 		DBHandler db;
 		try {
 			FloatingTask floatingTask = new FloatingTask("Name", "Desc");
+			FloatingTask floatingTask2 = new FloatingTask("Name", "Desc");
 			Date date = new Date();
 			DeadlineTask deadlineTask = new DeadlineTask(1, "Name", "Desc", date);
 			db = new DBHandler();
-			assertEquals("Delete", true, (db.deleteTask(1)));
 			assertEquals("Add task", 1, db.addTask(floatingTask));
-			// assertEquals("Update task", true, db.updateTask(deadlineTask));
+			assertEquals("Add task", 2, db.addTask(floatingTask2));
+			assertEquals("Update task", true, db.updateTask(deadlineTask));
 			assertEquals("Retreive", date,
 					((DeadlineTask) db.retrieveList(date).get(0)).getDeadline());
+			assertEquals("Delete", true, (db.deleteTask(1)));
+			assertEquals("Delete", true, (db.deleteTask(2)));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
