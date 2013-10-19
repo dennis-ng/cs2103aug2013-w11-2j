415dfe16bd4347b9b69f65f0877d6c7917836f25
Dennis Ng <Dennis Ng@localhost>
2013-10-19 21:37:28 +0800

    Sorting of tasks implemented

--- src/typetodo/model/DeadlineTask.java
+++ src/typetodo/model/DeadlineTask.java
@@ -1,21 +1,24 @@
 package typetodo.model;
 
+import java.util.Comparator;
+
 import org.joda.time.DateTime;
 
-public class DeadlineTask extends Task{
+public class DeadlineTask extends Task {
 	private DateTime deadline;
-	
+
 	public DeadlineTask(String name, String description, DateTime deadline) {
 		super(name, description);
 		this.setDeadline(deadline);
 	}
 
-	public DeadlineTask(int taskId, String name, String description, DateTime deadline) {
+	public DeadlineTask(int taskId, String name, String description,
+			DateTime deadline) {
 		super(name, description);
 		this.setTaskId(taskId);
 		this.setDeadline(deadline);
 	}
-	
+
 	public DateTime getDeadline() {
 		return deadline;
 	}
@@ -23,8 +26,16 @@ public class DeadlineTask extends Task{
 	public void setDeadline(DateTime deadline) {
 		this.deadline = deadline;
 	}
-	
+
 	public Task makeCopy() {
-		return new DeadlineTask(this.getTaskId(), this.getTitle(), this.getDescription(), this.getDeadline());
+		return new DeadlineTask(this.getTaskId(), this.getTitle(),
+				this.getDescription(), this.getDeadline());
 	}
+
+	public static final Comparator<DeadlineTask> COMPARE_BY_DATE = new Comparator<DeadlineTask>() {
+		@Override
+		public int compare(DeadlineTask task1, DeadlineTask task2) {
+			return task1.getDeadline().compareTo(task2.getDeadline());
+		}
+	};
 }
--- src/typetodo/model/TimedTask.java
+++ src/typetodo/model/TimedTask.java
@@ -1,27 +1,31 @@
 package typetodo.model;
 
+import java.util.Comparator;
+
 import org.joda.time.DateTime;
 
 public class TimedTask extends Task {
 	private DateTime start;
 	private DateTime end;
 	private boolean isBusy;
-	
-	public TimedTask(String name, String description, DateTime start, DateTime end, boolean isBusy) {
+
+	public TimedTask(String name, String description, DateTime start,
+			DateTime end, boolean isBusy) {
 		super(name, description);
 		this.setStart(start);
 		this.setEnd(end);
 		this.setBusy(isBusy);
 	}
 
-	public TimedTask(int taskId, String name, String description, DateTime start, DateTime end, boolean isBusy) {
+	public TimedTask(int taskId, String name, String description, DateTime start,
+			DateTime end, boolean isBusy) {
 		super(name, description);
 		this.setTaskId(taskId);
 		this.setStart(start);
 		this.setEnd(end);
 		this.setBusy(isBusy);
 	}
-	
+
 	public DateTime getStart() {
 		return start;
 	}
@@ -44,10 +48,21 @@ public class TimedTask extends Task {
 
 	public void setBusy(boolean isBusy) {
 		this.isBusy = isBusy;
-	}	
-	
-	public Task makeCopy() {
-		return new TimedTask(this.getTaskId(), this.getTitle(), this.getDescription(), this.getStart(),
-				this.getEnd(), this.isBusy());
 	}
+
+	public Task makeCopy() {
+		return new TimedTask(this.getTaskId(), this.getTitle(),
+				this.getDescription(), this.getStart(), this.getEnd(), this.isBusy());
+	}
+
+	public static final Comparator<TimedTask> COMPARE_BY_DATE = new Comparator<TimedTask>() {
+		@Override
+		public int compare(TimedTask task1, TimedTask task2) {
+			if (task1.getEnd().isEqual(task2.getEnd())) {
+				return task1.getStart().compareTo(task2.getStart());
+			} else {
+				return task1.getEnd().compareTo(task2.getEnd());
+			}
+		}
+	};
 }
