59f32e1bd78e14537fd6dcebdc545291967aedc6
Dennis Ng <Dennis Ng@localhost>
2013-11-04 20:48:48 +0800

    Major change to the GU in implementation of hotkeys: Global - ctrl+space: hide or restore In input box - ctrl+backspace: undo - up and down: traverse through history of inputs - esc: go into navigation mode for the list of tasks In list of tasks: - left key: scroll down by 1 page(full height) - right key: scroll up by 1 page(full height) Anywhere when app is in focus and not in input box - enter or "i": go back into input mode

--- src/typetodo/ui/TypeToDoGui.java
+++ src/typetodo/ui/TypeToDoGui.java
@@ -7,13 +7,17 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.IOException;
+import java.util.LinkedList;
 
+import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
@@ -34,21 +38,25 @@ public class TypeToDoGui extends JFrame implements View, NativeKeyListener,
 		WindowListener {
 
 	private static TypeToDoGui mainGui;
-	private static String cmd = null;
+	private static CommandPanel cmdPanel;
+	private static JTextField txtCmd;
 	private static FeedbackDialog feedbackDialog;
 	private static ScheduleController sc;
-	private static JTextField txtCmd;
+	private final LinkedList<String> inputHistory;
+	private int historyIndex;
 
 	private TypeToDoGui() {
+
+		inputHistory = new LinkedList<String>();
 
 		this.setUndecorated(true);
 		this.setLayout(new BorderLayout());
 		this.setBackground(new Color(0, 0, 0, 0));
 		this.setAlwaysOnTop(true);
 		addWindowListener(this);
-		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		CommandPanel cmdPanel = new CommandPanel();
+		cmdPanel = new CommandPanel();
 		cmdPanel.setFrameToMinimize(this);
 		cmdPanel.setFrameToClose(this);
 		cmdPanel.setBorder(new EmptyBorder(0, 5, 0, 5));
@@ -72,13 +80,74 @@ public class TypeToDoGui extends JFrame implements View, NativeKeyListener,
 		feedbackDialog.addMouseMotionListener(wma);
 		wma.addComponentToMove(feedbackDialog);
 
+		// TypeToDoGui handles the components that involves knowledge of
+		// ScheduleController to ensure only TypeToDoGui has the knowledge of
+		// ScheduleController
 		txtCmd = cmdPanel.getTxtCmd();
 		txtCmd.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JTextField source = (JTextField) e.getSource();
-				sc.parseAndExecute(source.getText());
-				source.setText("");
+				String input = txtCmd.getText();
+				if (!input.isEmpty()) {
+					sc.parseAndExecute(input);
+					if (historyIndex != 0) {
+						// The user had navigated into the history, thus we need to clear
+						// history.
+						inputHistory.pop();
+						historyIndex = 0; // Reset
+					}
+					addToHistory(input);
+					txtCmd.setText("");
+				}
+			}
+
+		});
+		txtCmd.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0),
+				"traverseBackInHistory");
+		txtCmd.getActionMap().put("traverseBackInHistory", new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				if (historyIndex == 0) {
+					addToHistory(txtCmd.getText());
+				}
+				if (historyIndex < inputHistory.size() - 1) {
+					historyIndex++;
+					txtCmd.setText(inputHistory.get(historyIndex));
+				}
+			}
+		});
+		txtCmd.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0),
+				"traverseFrontInHistory");
+		txtCmd.getActionMap().put("traverseFrontInHistory", new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				String history;
+				if (historyIndex > 0) {
+					historyIndex--;
+					if (historyIndex == 0) {
+						// Remove the last input that the user has not execute
+						history = inputHistory.pop();
+					} else {
+						history = inputHistory.get(historyIndex);
+					}
+					txtCmd.setText(history);
+				}
+			}
+		});
+		txtCmd.getInputMap()
+				.put(
+						KeyStroke.getKeyStroke(KeyEvent.VK_BACK_SPACE,
+								KeyEvent.CTRL_DOWN_MASK), "undo");
+		txtCmd.getActionMap().put("undo", new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				// Hotkey represents user typing undo
+				sc.parseAndExecute("undo");
+			}
+		});
+		txtCmd.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+				"moveToDialog");
+		txtCmd.getActionMap().put("moveToDialog", new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				feedbackDialog.toFront();
+				feedbackDialog.requestFocusInWindow();
 			}
 		});
 		this.setVisible(true);
@@ -140,7 +209,7 @@ public class TypeToDoGui extends JFrame implements View, NativeKeyListener,
 				&& e.getModifiers() == NativeKeyEvent.CTRL_MASK) {
 			SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
-					if (getState() == JFrame.ICONIFIED) {
+					if (getExtendedState() == JFrame.ICONIFIED) {
 						setExtendedState(JFrame.NORMAL);
 						toFront();
 						txtCmd.requestFocusInWindow();
@@ -149,6 +218,15 @@ public class TypeToDoGui extends JFrame implements View, NativeKeyListener,
 					}
 				}
 			});
+		} else if (e.getKeyCode() == NativeKeyEvent.VK_F4
+				&& e.getModifiers() == NativeKeyEvent.ALT_MASK) {
+			dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+		} else if (getExtendedState() == JFrame.NORMAL) {
+			if (!txtCmd.hasFocus()
+					&& (e.getKeyCode() == NativeKeyEvent.VK_I || e.getKeyCode() == NativeKeyEvent.VK_ENTER)) {
+				toFront();
+				txtCmd.requestFocusInWindow();
+			}
 		}
 	}
 
@@ -158,6 +236,24 @@ public class TypeToDoGui extends JFrame implements View, NativeKeyListener,
 
 	@Override
 	public void nativeKeyTyped(NativeKeyEvent e) { /* Unimplemented */
+		if (e.getKeyCode() == NativeKeyEvent.VK_I) {
+			if (getExtendedState() == JFrame.NORMAL && !txtCmd.hasFocus()) {
+				txtCmd.requestFocusInWindow();
+				System.out.println(e.getKeyChar());
+			}
+		}
+	}
+
+	/**
+	 * @param input
+	 *          We want to record a maximum of 50 histories so as not to waste
+	 *          memory.
+	 */
+	private void addToHistory(String input) {
+		if (inputHistory.size() > 50) {
+			inputHistory.pollLast();
+		}
+		inputHistory.push(input);
 	}
 
 	@Override
